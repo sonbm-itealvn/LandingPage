76a3bc9ce8d6866df1b9a41f5509c71a55bf5f4c
sonbm-itealvn <sonbm.itealvn@gmail.com>
2025-12-15 20:38:48 +0700

    fix reload 404

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import router from './router'
 
 const app = createApp(App)
 
+const redirectPath = sessionStorage.getItem('redirect')
+if (redirectPath) {
+  sessionStorage.removeItem('redirect')
+  router.replace(redirectPath)
+}
+
 app.use(router)
 
 app.mount('#app')
